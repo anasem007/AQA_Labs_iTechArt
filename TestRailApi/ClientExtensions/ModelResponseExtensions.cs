9b4f917b257077a2ff19c3c4555132cfdf747c97
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-10 14:26:29 +0300

    Add ClientExtensions.cs

---
+++ TestRailApi/ClientExtensions/ModelResponseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+using TestRailApi.Helpers;
+using TestRailApi.Models;
+using TestRailApi.Services;
+
+namespace TestRailApi.ClientExtensions
+{
+    public class ModelResponseExtensions<T>
+    {
+        public async Task<IRestResponse<T>> CreateResponse(string endPoint, Method method,
+            User user, object model = null)
+        {
+            var request = RestRequestBuilder.Build(endPoint, method, user, model);
+            var client = user.CreateClient();
+            
+            return ClientHelper.GetResponse<T>(client, request).Result;
+        }
+    }
+}
