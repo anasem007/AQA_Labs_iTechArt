5c57239710bdaab5af11c20012a9dcd852de126e
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-10 14:17:34 +0300

    Add Constants

---
+++ TestRailApi/Constants/AllureConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TestRailApi.Constants
+{
+    public class AllureConstants
+    {
+        public const string CONFIG_FILENAME = "allureConfig.json";        
+        public const string DEFAULT_RESULTS_FOLDER = "allure-results";
+    }
+}
